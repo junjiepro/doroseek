3a13c46822794b422ce61a7d1d4505b294c4a823
Junjie <2721795866@qq.com>
2025-02-12 23:38:07 +0800

    🎉 init:

---
+++ routes/index.tsx
@@ -0,0 +1,8 @@
+import { FreshContext } from "$fresh/server.ts";
+import * as base58 from "$std/encoding/base58.ts";
+
+export const handler = (req: Request, _ctx: FreshContext): Response => {
+  const listId = base58.encode(crypto.getRandomValues(new Uint8Array(8)));
+  const url = new URL(req.url);
+  return Response.redirect(`${url.origin}/${listId}`, 302);
+};
